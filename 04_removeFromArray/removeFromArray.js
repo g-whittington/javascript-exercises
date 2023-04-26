d6a03d5ac4e0c43c49687fc32b9e55712d0df2de
George <lwhittington2231@gmail.com>
2023-04-26 01:15:56 -0400

    Finished task 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    lenArray = array.length;
 
+    for (let arg of args) {
+        for (let i = 0; i < lenArray; i++) {
+            if (array[i] === arg) {
+                array.splice(i,1);
+            };
+        };
+    };
+
+    return array;
 };
 
 // Do not edit below this line
