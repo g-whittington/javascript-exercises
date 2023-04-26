721bb10dc2e977549faadc66a384b27bcdd5a193
George <lwhittington2231@gmail.com>
2023-04-26 00:37:06 -0400

    Finished task 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function(string, numTimes) {
+    if (numTimes < 0) return "ERROR";
+    let resultString = "";
 
+    for (let i = 0; i < numTimes; i++) {
+        resultString += string;
+    };
+    
+    return resultString;
 };
 
 // Do not edit below this line
