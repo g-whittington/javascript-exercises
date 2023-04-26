08163897dd11864bd2a4ec30a8d9643d8559acb3
George <lwhittington2231@gmail.com>
2023-04-26 00:47:20 -0400

    Finished task 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    let wordArr = word.split("");
+    let wordLength = word.length;
+    let reverseWord = "";
 
+    for (let i = 0; i < wordLength; i++){
+        reverseWord += wordArr.pop();
+    };
+
+    return reverseWord;
 };
 
 // Do not edit below this line
